f29a73518f8c65e6d00427149d8eba084abcb340
unknown <saounmustafa@gmail.com>
2021-10-28 23:58:54 +0500

    forth-commit

--- App.js
+++ App.js
@@ -23,29 +23,66 @@ const Board=()=>{
   const[sign8,changeSign8]=React.useState("")
   const[sign9,changeSign9]=React.useState("")
   const [player,playerTurn]=React.useState("player1")
+  let [arr,setArr]=React.useState([1,2,3,4,5,6,7,8,9])
+  
+  if((arr[0] == arr[1] && arr[1] == arr[2])||
+      (arr[0] == arr[3] && arr[3] == arr[6]) || 
+     (arr[0] == arr[4] && arr[4]==arr[8]) ||
+       (arr[1] == arr[4] && arr[4] == arr[7]) || 
+        (arr[2] == arr[4] && arr[4] == arr[6] )||
+         (arr[2] == arr[5]  && arr[5] == arr[8])|| 
+         (arr[3] == arr[4]  && arr[4]  == arr[5]) ||
+        (arr[6] == arr[7]  && arr[7]  == arr[8])){
+          if(player=== "player1"){
+            return(
+              <View>
+                <Text>
+                Winner is player 2
+                </Text>
+              </View>
+            )
+          
+          }
+          else { 
+            return(
+          <View>
+          <Text>
+          Winner is player 1
+          </Text>
+        </View>
+            )
+          }
+        }
+        
+ else{
+
   return(
   <View style={styles.board}>
+  
     <Text style={{fontSize:25,marginBottom:"10%",transform: [{ rotate: '180deg' }]}}>Player 2: 0</Text>
     <View style={styles.innerRows}>
-  <Pressable style={styles.btns} onPress={()=>changeSign(checkSign(sign,player,playerTurn))} ><Text>{sign}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign2(checkSign(sign2,player,playerTurn))}><Text>{sign2}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign3(checkSign(sign3,player,playerTurn))}><Text>{sign3}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign(checkSign(sign,player,playerTurn,checkWin(1,arr,setArr,checkSign(sign,player,playerTurn))))}><Text>{sign}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign2(checkSign(sign2,player,playerTurn,checkWin(2,arr,setArr,checkSign(sign2,player,playerTurn))))}><Text>{sign2}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign3(checkSign(sign3,player,playerTurn,checkWin(3,arr,setArr,checkSign(sign3,player,playerTurn))))}><Text>{sign3}</Text></Pressable>
     </View>
 
     <View style={styles.innerRows}>
-  <Pressable style={styles.btns} onPress={()=>changeSign4(checkSign(sign4,player,playerTurn))} ><Text>{sign4}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign5(checkSign(sign5,player,playerTurn))}><Text>{sign5}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign6(checkSign(sign6,player,playerTurn))}><Text>{sign6}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign4(checkSign(sign4,player,playerTurn),checkWin(4,arr,setArr,checkSign(sign4,player,playerTurn)))} ><Text>{sign4}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign5(checkSign(sign5,player,playerTurn),checkWin(5,arr,setArr,checkSign(sign5,player,playerTurn)))}><Text>{sign5}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign6(checkSign(sign6,player,playerTurn),checkWin(6,arr,setArr,checkSign(sign6,player,playerTurn)))}><Text>{sign6}</Text></Pressable>
     </View>
 
     <View style={styles.innerRows}>
-  <Pressable style={styles.btns} onPress={()=>changeSign7(checkSign(sign7,player,playerTurn))} ><Text>{sign7}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign8(checkSign(sign8,player,playerTurn))}><Text>{sign8}</Text></Pressable>
-  <Pressable style={styles.btns} onPress={()=>changeSign9(checkSign(sign9,player,playerTurn))}><Text>{sign9}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign7(checkSign(sign7,player,playerTurn),checkWin(7,arr,setArr,checkSign(sign7,player,playerTurn)))} ><Text>{sign7}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign8(checkSign(sign8,player,playerTurn),checkWin(8,arr,setArr,checkSign(sign8,player,playerTurn)))}><Text>{sign8}</Text></Pressable>
+  <Pressable style={styles.btns} onPress={()=>changeSign9(checkSign(sign9,player,playerTurn),checkWin(9,arr,setArr,checkSign(sign9,player,playerTurn)))}><Text>{sign9}</Text></Pressable>
     </View>
     <Text style={{fontSize:25,marginTop:"10%"}}>Player 1: X</Text>
   </View>)
-}
+}}
+
+
+
 const checkSign=(sign,player,playerTurn)=>{
   
   if(sign==="" && player==="player1"){
@@ -54,21 +91,24 @@ const checkSign=(sign,player,playerTurn)=>{
 }
 else if(sign==="" && player==="player2"){
   playerTurn("player1")
-return "0"
+return "O"
 }
 else{
-  return sign;
+return sign;
 }}
 
+const checkWin=(num,arr,setArr,sign)=>{
+  console.log(sign)
+  console.log(num);
+ arr[num-1]=sign;
+ setArr(arr);
+ for(var i=0;i<arr.length;i++){
+    console.log("At index "+i+" = " + arr[i])
+  
+   }
+}
 
 
-const changePlayer=(player)=>{
-  if(player==="player1")
-  return "player2"
-else{
-  return "player1"
-}
-}
 
 const styles = StyleSheet.create({
   container: {
